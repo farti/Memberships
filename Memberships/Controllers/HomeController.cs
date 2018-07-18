fd67438f958b3152d506748d8e5dbac6a033e54f
Marcin Rakoczy <mrakoczy@wp.pl>
2018-07-18 14:55:40 +0200

    Added Thumbnail Views

--- Memberships/Controllers/HomeController.cs
+++ Memberships/Controllers/HomeController.cs
@@ -1,22 +1,31 @@
-﻿using Memberships.Models;
+﻿using System;
+using Memberships.Models;
 using Microsoft.AspNet.Identity;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using Memberships.Extensions;
 
 namespace Memberships.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
             var userId = Request.IsAuthenticated ? HttpContext.User.Identity.GetUserId() : null;
-
+            var thumbnails = await new List<ThumbnailModel>().GetProductThumbnailsAsync(userId);
+            var count = thumbnails.Count() / 4;
             var model = new List<ThumbnailAreaModel>();
-            model.Add(new ThumbnailAreaModel
+            for (int i = 0; i <= count; i++)
             {
-                Title = "Area title",
-                Thumbnails = new List<ThumbnailModel>()
-            });
+                model.Add(new ThumbnailAreaModel
+                {
+                    Title = i.Equals(0) ? "My content" : String.Empty,
+                    Thumbnails = thumbnails.Skip(i * 4).Take(4)
+                });
+            }
+
             return View(model);
         }
 
